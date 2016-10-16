809f6d1b54f712a92df5149a938ec1d63f0c05ee
Cecilia <cecilia_silvera@hotmail.com>
2016-10-16 12:17:32 +0200

    add

---
+++ Scripts/Components/Board/board.component.js
@@ -0,0 +1,24 @@
+﻿angular.module("baseModule")
+    .component("board", {
+        templateUrl: "Scripts/Components/Board/Board.html",
+        controller: function () {
+            var scope = this;
+            var component = {};
+            console.log("1223")
+            setTimeout(function () {
+                component = $("#board-" + scope.id);
+                component.droppable({
+                    drop: function (event, ui) {
+                        //console.log("Dropped in " + scope.id);
+                        console.log(1789);
+                        console.log(ui.note);
+                    }
+                });
+            },100);
+
+        },
+        controllerAs: "scope",
+        bindings: {
+            id: "="
+        }
+    });
---
+++ Scripts/Components/Note/note.component.js
@@ -0,0 +1,17 @@
+﻿angular.module("baseModule")
+    .component("note", {
+        templateUrl: "Scripts/Components/Note/Note.html",
+        controller: function () {
+            var scope = this;
+            var component = {};
+            setTimeout(function () {
+                component = $("#note-" + scope.id);
+                component.draggable();
+            }, 100);
+        },
+        controllerAs: "scope",
+        bindings: {
+            id: "=",
+            body: "="
+        }
+    });
